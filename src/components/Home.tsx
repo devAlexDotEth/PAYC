e0cd60f42bd36d22db9d6191f06ea1cc67a1f7b4
devAlex.ᴱᵀᴴ <88956822+devAlexDotEth@users.noreply.github.com>
2023-10-22 21:05:58 -0400

    PushGPT

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            {/* Components and structure here */}
+        </div>
+    );
+};
+
+export default Home;
